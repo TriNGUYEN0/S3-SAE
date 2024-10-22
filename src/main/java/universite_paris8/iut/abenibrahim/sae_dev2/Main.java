797412262c808b2e29e6c480c4e13a18d6916ee3
yk18b <ykonan@iut.univ-paris8.fr>
2024-10-22 14:04:08 +0200

    singleton Environnements

--- src/main/java/universite_paris8/iut/abenibrahim/sae_dev2/Main.java
+++ src/main/java/universite_paris8/iut/abenibrahim/sae_dev2/Main.java
@@ -10,6 +10,7 @@ import universite_paris8.iut.abenibrahim.sae_dev2.modele.Environnement;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("lafoudre.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),750,750);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
